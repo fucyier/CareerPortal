ee75423b1c4e9c591aaa08eb315a0df8c4b34b8d
Remix Workspace <>
2022-09-20 18:27:02 +0300

    ilan bilgileri refactoring

--- contracts/IlanBilgileri.sol
+++ contracts/IlanBilgileri.sol
@@ -9,7 +9,6 @@ contract IlanBilgileri is BaseContract{
         uint public id;
 
         struct IlanBilgi {
-         uint id;
          address talepEdenKurum;
          uint32 pozisyon;
          uint8 sektor;
@@ -19,7 +18,7 @@ contract IlanBilgileri is BaseContract{
          uint ilanBasTarih;
          uint ilanBitTarih;
          uint8 tecrubeYili;
-        // uint8 ulke;
+         uint8 ulke;
          uint32 sehir;
         }
        
@@ -29,12 +28,12 @@ contract IlanBilgileri is BaseContract{
         event IlanBilgiGuncellendiLog(uint _id, address _talepEdenKurum, uint32 _pozisyon, uint8 _sektor);
         
 
-  function ekleIlanBilgi(address _talepEdenKurum, uint32 _pozisyon, uint8 _sektor, CalismaTipi _calismaTipi, string memory _arananOzellikler, string memory _isTanimi, uint _ilanBasTarih, uint _ilanBitTarih, 
-                        uint32 _sehir, uint8 _tecrubeYili)  public sadece_Uni_Firma_Kamu{
-             require(kamuKurumlari[_talepEdenKurum].durum||firmalar[_talepEdenKurum].durum,"Ilan acan kurum kayitli degil");
+  function ekleIlanBilgi( uint32 _pozisyon, uint8 _sektor, CalismaTipi _calismaTipi, string memory _arananOzellikler, string memory _isTanimi, uint _ilanBasTarih, uint _ilanBitTarih, 
+                        uint8 _ulke, uint32 _sehir, uint8 _tecrubeYili)  public sadece_Uni_Firma_Kamu{
+             require(kamuKurumlari[msg.sender].durum||firmalar[msg.sender].durum,"Ilan acan kurum kayitli degil");
              uint yeniId=id++;
             
-             ilanBilgileri[yeniId].talepEdenKurum=_talepEdenKurum;
+             ilanBilgileri[yeniId].talepEdenKurum=msg.sender;
              ilanBilgileri[yeniId].pozisyon=_pozisyon;
              ilanBilgileri[yeniId].sektor=_sektor;
              ilanBilgileri[yeniId].calismaTipi=_calismaTipi;
@@ -43,17 +42,17 @@ contract IlanBilgileri is BaseContract{
              ilanBilgileri[yeniId].ilanBasTarih=_ilanBasTarih;
              ilanBilgileri[yeniId].ilanBitTarih=_ilanBitTarih;
              ilanBilgileri[yeniId].tecrubeYili=_tecrubeYili;
-            // ilanBilgileri[yeniId].ulke=_ulke;
+             ilanBilgileri[yeniId].ulke=_ulke;
              ilanBilgileri[yeniId].sehir=_sehir;
 
            
-             emit IlanBilgiEklendiLog( yeniId,_talepEdenKurum, _pozisyon,_sektor );
+             emit IlanBilgiEklendiLog( yeniId,msg.sender, _pozisyon,_sektor );
     }
-       function guncelleIlanBilgi(uint _ilanId, address _talepEdenKurum, uint32 _pozisyon, uint8 _sektor, CalismaTipi _calismaTipi, string memory _arananOzellikler, string memory _isTanimi, uint _ilanBasTarih, uint _ilanBitTarih, 
-                        uint32 _sehir, uint8 _tecrubeYili)  public sadece_Uni_Firma_Kamu returns(uint){
-            require(kamuKurumlari[_talepEdenKurum].durum||firmalar[_talepEdenKurum].durum,"Ilan acan kurum kayitli degil");
+       function guncelleIlanBilgi(uint _ilanId, uint32 _pozisyon, uint8 _sektor, CalismaTipi _calismaTipi, string memory _arananOzellikler, string memory _isTanimi, uint _ilanBasTarih, uint _ilanBitTarih, 
+                        uint8 _ulke,uint32 _sehir, uint8 _tecrubeYili)  public sadece_Uni_Firma_Kamu returns(uint){
+            require(kamuKurumlari[msg.sender].durum||firmalar[msg.sender].durum,"Ilan acan kurum kayitli degil");
              
-             ilanBilgileri[_ilanId].talepEdenKurum=_talepEdenKurum;
+             ilanBilgileri[_ilanId].talepEdenKurum=msg.sender;
              ilanBilgileri[_ilanId].pozisyon=_pozisyon;
              ilanBilgileri[_ilanId].sektor=_sektor;
              ilanBilgileri[_ilanId].calismaTipi=_calismaTipi;
@@ -62,10 +61,10 @@ contract IlanBilgileri is BaseContract{
              ilanBilgileri[_ilanId].ilanBasTarih=_ilanBasTarih;
              ilanBilgileri[_ilanId].ilanBitTarih=_ilanBitTarih;
              ilanBilgileri[_ilanId].tecrubeYili=_tecrubeYili;
-             //ilanBilgileri[_ilanId].ulke=_ulke;
+             ilanBilgileri[_ilanId].ulke=_ulke;
              ilanBilgileri[_ilanId].sehir=_sehir;
            
-             emit IlanBilgiGuncellendiLog( _ilanId,_talepEdenKurum, _pozisyon,_sektor );
+             emit IlanBilgiGuncellendiLog( _ilanId,msg.sender, _pozisyon,_sektor );
              return _ilanId;
 
     }
